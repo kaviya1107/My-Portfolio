b8846b2e3a0e2499395b33e679b5df8c7f3f07ed
kaviya1107 <kavithakaviya0326@gmail.com>
2024-10-22 17:55:30 +0530

    add

---
+++ src/app/models/project.ts
@@ -0,0 +1,9 @@
+export interface Project{
+    id:number;
+    name:string;
+    summary:string;
+    description:string;
+    projectlink:string;
+    pictures:string[];
+    tags:string[];
+}
--- src/app/project-card/project-card.component.ts
+++ src/app/project-card/project-card.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Project } from '../models/project';
 
 @Component({
   selector: 'app-project-card',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./project-card.component.css']
 })
 export class ProjectCardComponent {
-
+  @Input() project={} as Project
 }
